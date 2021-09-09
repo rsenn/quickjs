51b5bc6ac74415c7772adfee075cf7c56ebfb01b
Roman Senn <roman.l.senn@gmail.com>
2021-09-09 09:54:27 +0200

    ...

--- cmake/MakeQuickJSModule.cmake
+++ cmake/MakeQuickJSModule.cmake
@@ -28,9 +28,9 @@ function(make_module FNAME)
       COMPILE_FLAGS "${MODULE_COMPILE_FLAGS}")
   set_target_properties(${TARGET_NAME}-static PROPERTIES OUTPUT_NAME "${VNAME}" COMPILE_FLAGS "")
   target_compile_definitions(${TARGET_NAME} PRIVATE JS_SHARED_LIBRARY=1 JS_${UNAME}_MODULE=1
-                                                    CONFIG_PREFIX="${QUICKJS_PREFIX}")
+                                                    CONFIG_PREFIX="${QUICKJS_INSTALL_PREFIX}")
   target_compile_definitions(${TARGET_NAME}-static PRIVATE JS_${UNAME}_MODULE=1
-                                                           CONFIG_PREFIX="${QUICKJS_PREFIX}")
+                                                           CONFIG_PREFIX="${QUICKJS_INSTALL_PREFIX}")
   install(TARGETS ${TARGET_NAME} DESTINATION lib/quickjs
           PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ
                       WORLD_EXECUTE)
