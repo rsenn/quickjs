4684a017b047956c47f4b55af992c8bf85832af0
Roman Senn <roman.l.senn@gmail.com>
2025-03-09 10:35:56 +0100

    ...

--- libunicode.h
+++ libunicode.h
@@ -25,6 +25,7 @@
 #define LIBUNICODE_H
 
 #include <inttypes.h>
+#include "cutils.h"
 
 #define LRE_BOOL  int       /* for documentation purposes */
 
@@ -41,6 +42,7 @@ typedef enum {
 } UnicodeNormalizationEnum;
 
 int lre_case_conv(uint32_t *res, uint32_t c, int conv_type);
+int lre_canonicalize(uint32_t c, BOOL is_unicode);
 LRE_BOOL lre_is_cased(uint32_t c);
 LRE_BOOL lre_is_case_ignorable(uint32_t c);
 
@@ -100,6 +102,8 @@ int cr_op(CharRange *cr, const uint32_t *a_pt, int a_len,
           const uint32_t *b_pt, int b_len, int op);
 
 int cr_invert(CharRange *cr);
+
+int cr_regexp_canonicalize(CharRange *cr, BOOL is_unicode);
 
 #ifdef CONFIG_ALL_UNICODE
 
