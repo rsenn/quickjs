fbe97bee04f662375cd7c2452e7f2cfd41407782
Roman Senn <roman.l.senn@gmail.com>
2025-04-12 21:14:16 +0200

    ...

--- qjs-sound/webaudio-test.js
+++
@@ -1,9 +0,0 @@
-let context = new AudioContext();
-
-let d = context.destination;
-
-let o = new OscillatorNode(context, { type: 'sawtooth', frequency: 1000, channelCount: 2 });
-
-o.connect(d);
-o.start();
-setTimeout(() => o.stop(), 1000);
