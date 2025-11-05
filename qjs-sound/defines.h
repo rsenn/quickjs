73fc0ab4921fcc6f0c41aeae15798d6b164f65a1
Roman Senn <roman.l.senn@gmail.com>
2025-11-05 04:47:20 +0100

    ...

--- qjs-sound/defines.h
+++
@@ -1,9 +0,0 @@
-
-#if defined(_WIN32) || defined(__MINGW32__)
-#define VISIBLE __declspec(dllexport)
-#define HIDDEN
-#else
-#define VISIBLE __attribute__((visibility("default")))
-#define HIDDEN __attribute__((visibility("hidden")))
-#endif
-
--- qjs-sound/webaudio-test.js
+++
@@ -1,9 +0,0 @@
-context = new AudioContext();
-
-d = context.destination;
-
-o = new OscillatorNode(context, { type: 'sawtooth', frequency: 1000, channelCount: 2 });
-
-o.connect(d);
-o.start();
-setTimeout(() => o.stop(), 1000);
