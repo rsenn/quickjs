7cc0e5878e8a5255542651b2cc6320ae30f14d4e
Roman Senn <roman.l.senn@gmail.com>
2026-07-24 07:06:01 +0200

    ...

--- repl.js
+++ repl.js
@@ -1,5 +1,3 @@
-import * as os from 'os';
-import * as std from 'std';
 /*
  * QuickJS Read Eval Print Loop
  *
@@ -24,7 +22,9 @@ import * as std from 'std';
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-('use strip');
+import * as std from 'std';
+import * as os from 'os';
+
 (function (g) {
   /* add 'os' and 'std' bindings */
   g.os = os;
@@ -38,11 +38,6 @@ import * as std from 'std';
   var Math = g.Math;
   var isFinite = g.isFinite;
   var parseFloat = g.parseFloat;
-
-  /* XXX: use preprocessor ? */
-  var config_numcalc = typeof os.open === 'undefined';
-  var has_jscalc = typeof Fraction === 'function';
-  var has_bignum = typeof BigFloat === 'function';
 
   var colors = {
     none: '\x1b[0m',
@@ -65,38 +60,20 @@ import * as std from 'std';
     bright_white: '\x1b[37;1m',
   };
 
-  var styles;
-  if(config_numcalc) {
-    styles = {
-      default: 'black',
-      comment: 'white',
-      string: 'green',
-      regex: 'cyan',
-      number: 'green',
-      keyword: 'blue',
-      function: 'gray',
-      type: 'bright_magenta',
-      identifier: 'yellow',
-      error: 'bright_red',
-      result: 'black',
-      error_msg: 'bright_red',
-    };
-  } else {
-    styles = {
-      default: 'bright_green',
-      comment: 'white',
-      string: 'bright_cyan',
-      regex: 'cyan',
-      number: 'green',
-      keyword: 'bright_white',
-      function: 'bright_yellow',
-      type: 'bright_magenta',
-      identifier: 'bright_green',
-      error: 'red',
-      result: 'bright_white',
-      error_msg: 'bright_red',
-    };
-  }
+  var styles = {
+    default: 'bright_green',
+    comment: 'white',
+    string: 'bright_cyan',
+    regex: 'cyan',
+    number: 'green',
+    keyword: 'bright_white',
+    function: 'bright_yellow',
+    type: 'bright_magenta',
+    identifier: 'bright_green',
+    error: 'red',
+    result: 'bright_white',
+    error_msg: 'bright_red',
+  };
 
   var history = [];
   var clip_board = '';
@@ -107,13 +84,12 @@ import * as std from 'std';
   var pstate = '';
   var prompt = '';
   var plen = 0;
-  var ps1;
-  if(config_numcalc) ps1 = '> ';
-  else ps1 = 'qjs > ';
+  var ps1 = 'qjs > ';
   var ps2 = '  ... ';
   var utf8 = true;
   var show_time = false;
   var show_colors = true;
+  var eval_start_time;
   var eval_time = 0;
 
   var mexpr = '';
@@ -569,7 +545,7 @@ import * as std from 'std';
       obj = {};
       switch ((c = line[pos - 1])) {
         case "'":
-        case '"':
+        case '\"':
           return 'a';
         case ']':
           return [];
@@ -581,6 +557,8 @@ import * as std from 'std';
           if(is_word(c)) {
             base = get_context_word(line, pos);
             if(['true', 'false', 'null', 'this'].includes(base) || !isNaN(+base)) return eval(base);
+            // Check if `base` is a set of regexp flags
+            if(pos - base.length >= 3 && line[pos - base.length - 1] === '/') return new RegExp('', base);
             obj = get_context_object(line, pos - base.length);
             if(obj === null || obj === void 0) return obj;
             if(obj === g && obj[base] === void 0) return eval(base);
@@ -767,10 +745,8 @@ import * as std from 'std';
       prompt += ps2;
     } else {
       if(show_time) {
-        var t = Math.round(eval_time) + ' ';
-        eval_time = 0;
-        t = dupstr('0', 5 - t.length) + t;
-        prompt += t.substring(0, t.length - 4) + '.' + t.substring(t.length - 4);
+        var t = eval_time / 1000;
+        prompt += t.toFixed(6) + ' ';
       }
       plen = prompt.length;
       prompt += ps1;
@@ -854,163 +830,17 @@ import * as std from 'std';
   }
 
   var hex_mode = false;
-  var eval_mode = 'std';
 
-  function number_to_string(a, radix) {
+  function number_to_string_hex(a) {
     var s;
-    if(!isFinite(a)) {
-      /* NaN, Infinite */
-      return a.toString();
+    if(a < 0) {
+      a = -a;
+      s = '-';
     } else {
-      if(a == 0) {
-        if(1 / a < 0) s = '-0';
-        else s = '0';
-      } else {
-        if(radix == 16 && a === Math.floor(a)) {
-          var s;
-          if(a < 0) {
-            a = -a;
-            s = '-';
-          } else {
-            s = '';
-          }
-          s += '0x' + a.toString(16);
-        } else {
-          s = a.toString();
-        }
-      }
-      return s;
+      s = '';
     }
-  }
-
-  function bigfloat_to_string(a, radix) {
-    var s;
-    if(!BigFloat.isFinite(a)) {
-      /* NaN, Infinite */
-      if(eval_mode !== 'math') {
-        return 'BigFloat(' + a.toString() + ')';
-      } else {
-        return a.toString();
-      }
-    } else {
-      if(a == 0) {
-        if(1 / a < 0) s = '-0';
-        else s = '0';
-      } else {
-        if(radix == 16) {
-          var s;
-          if(a < 0) {
-            a = -a;
-            s = '-';
-          } else {
-            s = '';
-          }
-          s += '0x' + a.toString(16);
-        } else {
-          s = a.toString();
-        }
-      }
-      if(typeof a === 'bigfloat' && eval_mode !== 'math') {
-        s += 'l';
-      } else if(eval_mode !== 'std' && s.indexOf('.') < 0 && ((radix == 16 && s.indexOf('p') < 0) || (radix == 10 && s.indexOf('e') < 0))) {
-        /* add a decimal point so that the floating point type
-                   is visible */
-        s += '.0';
-      }
-      return s;
-    }
-  }
-
-  function bigint_to_string(a, radix) {
-    var s;
-    if(radix == 16) {
-      var s;
-      if(a < 0) {
-        a = -a;
-        s = '-';
-      } else {
-        s = '';
-      }
-      s += '0x' + a.toString(16);
-    } else {
-      s = a.toString();
-    }
-    if(eval_mode === 'std') s += 'n';
+    s += '0x' + a.toString(16);
     return s;
-  }
-
-  function print(a) {
-    var stack = [];
-
-    function print_rec(a) {
-      var n, i, keys, key, type, s;
-
-      type = typeof a;
-      if(type === 'object') {
-        if(a === null) {
-          std.puts(a);
-        } else if(stack.indexOf(a) >= 0) {
-          std.puts('[circular]');
-        } else if(
-          has_jscalc &&
-          (a instanceof Fraction || a instanceof Complex || a instanceof Mod || a instanceof Polynomial || a instanceof PolyMod || a instanceof RationalFunction || a instanceof Series)
-        ) {
-          std.puts(a.toString());
-        } else {
-          stack.push(a);
-          if(Array.isArray(a)) {
-            n = a.length;
-            std.puts('[ ');
-            for(i = 0; i < n; i++) {
-              if(i !== 0) std.puts(', ');
-              if(i in a) {
-                print_rec(a[i]);
-              } else {
-                std.puts('<empty>');
-              }
-              if(i > 20) {
-                std.puts('...');
-                break;
-              }
-            }
-            std.puts(' ]');
-          } else if(Object.__getClass(a) === 'RegExp') {
-            std.puts(a.toString());
-          } else {
-            keys = Object.keys(a);
-            n = keys.length;
-            std.puts('{ ');
-            for(i = 0; i < n; i++) {
-              if(i !== 0) std.puts(', ');
-              key = keys[i];
-              std.puts(key, ': ');
-              print_rec(a[key]);
-            }
-            std.puts(' }');
-          }
-          stack.pop(a);
-        }
-      } else if(type === 'string') {
-        s = a.__quote();
-        if(s.length > 79) s = s.substring(0, 75) + '..."';
-        std.puts(s);
-      } else if(type === 'number') {
-        std.puts(number_to_string(a, hex_mode ? 16 : 10));
-      } else if(type === 'bigint') {
-        std.puts(bigint_to_string(a, hex_mode ? 16 : 10));
-      } else if(type === 'bigfloat') {
-        std.puts(bigfloat_to_string(a, hex_mode ? 16 : 10));
-      } else if(type === 'bigdecimal') {
-        std.puts(a.toString() + 'm');
-      } else if(type === 'symbol') {
-        std.puts(String(a));
-      } else if(type === 'function') {
-        std.puts('function ' + a.name + '()');
-      } else {
-        std.puts(a);
-      }
-    }
-    print_rec(a);
   }
 
   function extract_directive(a) {
@@ -1039,94 +869,15 @@ import * as std from 'std';
       hex_mode = false;
     } else if(cmd === 't') {
       show_time = !show_time;
-    } else if(has_bignum && cmd === 'p') {
-      param = expr
-        .substring(cmd.length + 1)
-        .trim()
-        .split(' ');
-      if(param.length === 1 && param[0] === '') {
-        std.puts('BigFloat precision=' + prec + ' bits (~' + Math.floor(prec / log2_10) + ' digits), exponent size=' + expBits + ' bits\n');
-      } else if(param[0] === 'f16') {
-        prec = 11;
-        expBits = 5;
-      } else if(param[0] === 'f32') {
-        prec = 24;
-        expBits = 8;
-      } else if(param[0] === 'f64') {
-        prec = 53;
-        expBits = 11;
-      } else if(param[0] === 'f128') {
-        prec = 113;
-        expBits = 15;
-      } else {
-        prec1 = parseInt(param[0]);
-        if(param.length >= 2) expBits1 = parseInt(param[1]);
-        else expBits1 = BigFloatEnv.expBitsMax;
-        if(Number.isNaN(prec1) || prec1 < BigFloatEnv.precMin || prec1 > BigFloatEnv.precMax) {
-          std.puts('Invalid precision\n');
-          return false;
-        }
-        if(Number.isNaN(expBits1) || expBits1 < BigFloatEnv.expBitsMin || expBits1 > BigFloatEnv.expBitsMax) {
-          std.puts('Invalid exponent bits\n');
-          return false;
-        }
-        prec = prec1;
-        expBits = expBits1;
-      }
-      return false;
-    } else if(has_bignum && cmd === 'digits') {
-      param = expr.substring(cmd.length + 1).trim();
-      prec1 = Math.ceil(parseFloat(param) * log2_10);
-      if(prec1 < BigFloatEnv.precMin || prec1 > BigFloatEnv.precMax) {
-        std.puts('Invalid precision\n');
-        return false;
-      }
-      prec = prec1;
-      expBits = BigFloatEnv.expBitsMax;
-      return false;
-    } else if(has_bignum && cmd === 'mode') {
-      param = expr.substring(cmd.length + 1).trim();
-      if(param === '') {
-        std.puts('Running mode=' + eval_mode + '\n');
-      } else if(param === 'std' || param === 'math') {
-        eval_mode = param;
-      } else {
-        std.puts('Invalid mode\n');
-      }
-      return false;
     } else if(cmd === 'clear') {
       std.puts('\x1b[H\x1b[J');
     } else if(cmd === 'q') {
       std.exit(0);
-    } else if(has_jscalc && cmd === 'a') {
-      algebraicMode = true;
-    } else if(has_jscalc && cmd === 'n') {
-      algebraicMode = false;
     } else {
       std.puts('Unknown directive: ' + cmd + '\n');
       return false;
     }
     return true;
-  }
-
-  if(config_numcalc) {
-    /* called by the GUI */
-    g.execCmd = function(cmd) {
-      switch (cmd) {
-        case 'dec':
-          hex_mode = false;
-          break;
-        case 'hex':
-          hex_mode = true;
-          break;
-        case 'num':
-          algebraicMode = false;
-          break;
-        case 'alg':
-          algebraicMode = true;
-          break;
-      }
-    };
   }
 
   function help() {
@@ -1144,67 +895,13 @@ import * as std from 'std';
         '\\t         ' +
         sel(show_time) +
         'toggle timing display\n' +
-        '\\clear      clear the terminal\n',
+        '\\clear      clear the terminal\n' +
+        '\\q          exit\n',
     );
-    if(has_jscalc) {
-      std.puts('\\a         ' + sel(algebraicMode) + 'algebraic mode\n' + '\\n         ' + sel(!algebraicMode) + 'numeric mode\n');
-    }
-    if(has_bignum) {
-      std.puts("\\p [m [e]]  set the BigFloat precision to 'm' bits\n" + "\\digits n   set the BigFloat precision to 'ceil(n*log2(10))' bits\n");
-      if(!has_jscalc) {
-        std.puts('\\mode [std|math] change the running mode (current = ' + eval_mode + ')\n');
-      }
-    }
-    if(!config_numcalc) {
-      std.puts('\\q          exit\n');
-    }
-  }
-
-  function eval_and_print(expr) {
-    var result;
-
-    try {
-      if(eval_mode === 'math') expr = '"use math"; void 0;' + expr;
-      var now = new Date().getTime();
-      /* eval as a script */
-      result = std.evalScript(expr, { backtrace_barrier: true });
-      eval_time = new Date().getTime() - now;
-      std.puts(colors[styles.result]);
-      print(result);
-      std.puts('\n');
-      std.puts(colors.none);
-      /* set the last result */
-      g._ = result;
-    } catch(error) {
-      std.puts(colors[styles.error_msg]);
-      if(error instanceof Error) {
-        console.log(error);
-        if(error.stack) {
-          std.puts(error.stack);
-        }
-      } else {
-        std.puts('Throw: ');
-        console.log(error);
-      }
-      std.puts(colors.none);
-    }
   }
 
   function cmd_start() {
-    if(!config_numcalc) {
-      if(has_jscalc) std.puts('QJSCalc - Type "\\h" for help\n');
-      else std.puts('QuickJS - Type "\\h" for help\n');
-    }
-    if(has_bignum) {
-      log2_10 = Math.log(10) / Math.log(2);
-      prec = 113;
-      expBits = 15;
-      if(has_jscalc) {
-        eval_mode = 'math';
-        /* XXX: numeric mode should always be the default ? */
-        g.algebraicMode = config_numcalc;
-      }
-    }
+    std.puts('QuickJS - Type "\\h" for help\n');
 
     cmd_readline_start();
   }
@@ -1214,27 +911,31 @@ import * as std from 'std';
   }
 
   function readline_handle_cmd(expr) {
-    handle_cmd(expr);
-    cmd_readline_start();
+    if(!handle_cmd(expr)) {
+      cmd_readline_start();
+    }
   }
 
+  /* return true if async termination */
   function handle_cmd(expr) {
     var colorstate, cmd;
 
     if(expr === null) {
       expr = '';
-      return;
+      return false;
     }
     if(expr === '?') {
       help();
-      return;
+      return false;
     }
     cmd = extract_directive(expr);
     if(cmd.length > 0) {
-      if(!handle_directive(cmd, expr)) return;
+      if(!handle_directive(cmd, expr)) {
+        return false;
+      }
       expr = expr.substring(cmd.length + 1);
     }
-    if(expr === '') return;
+    if(expr === '') return false;
 
     if(mexpr) expr = mexpr + '\n' + expr;
     colorstate = colorize_js(expr);
@@ -1242,19 +943,71 @@ import * as std from 'std';
     level = colorstate[1];
     if(pstate) {
       mexpr = expr;
-      return;
+      return false;
     }
     mexpr = '';
 
-    if(has_bignum) {
-      BigFloatEnv.setPrec(eval_and_print.bind(null, expr), prec, expBits);
-    } else {
-      eval_and_print(expr);
-    }
-    level = 0;
+    eval_and_print_start(expr);
 
+    return true;
+  }
+
+  function eval_and_print_start(expr) {
+    var result;
+
+    try {
+      eval_start_time = os.now();
+      /* eval as a script */
+      result = std.evalScript(expr, { backtrace_barrier: true, async: true });
+      /* result is a promise */
+      result.then(print_eval_result, print_eval_error);
+    } catch(error) {
+      print_eval_error(error);
+    }
+  }
+
+  function print_eval_result(result) {
+    var default_print = true;
+
+    result = result.value;
+    eval_time = os.now() - eval_start_time;
+    std.puts(colors[styles.result]);
+    if(hex_mode) {
+      if(typeof result == 'number' && result === Math.floor(result)) {
+        std.puts(number_to_string_hex(result));
+        default_print = false;
+      } else if(typeof result == 'bigint') {
+        std.puts(number_to_string_hex(result));
+        std.puts('n');
+        default_print = false;
+      }
+    }
+    if(default_print) {
+      std.__printObject(result);
+    }
+    std.puts('\n');
+    std.puts(colors.none);
+    /* set the last result */
+    g._ = result;
+
+    handle_cmd_end();
+  }
+
+  function print_eval_error(error) {
+    std.puts(colors[styles.error_msg]);
+    if(!(error instanceof Error)) std.puts('Throw: ');
+    std.__printObject(error);
+    std.puts('\n');
+    std.puts(colors.none);
+
+    handle_cmd_end();
+  }
+
+  function handle_cmd_end() {
+    level = 0;
     /* run the garbage collector after each command */
     std.gc();
+    cmd_readline_start();
   }
 
   function colorize_js(str) {
@@ -1449,7 +1202,7 @@ import * as std from 'std';
           can_regex = 1;
           continue;
         case "'":
-        case '"':
+        case '\"':
         case '`':
           parse_string(c);
           can_regex = 0;
